f1aa6c8a9111a0387723ba07f45186e188a6d9e3
choidongkuen <danaver12@daum.net>
2024-03-20 21:51:21 +0900

    chore : 프로젝트 세팅

---
+++ src/main/java/com/metlife/MetlifeTeam5Application.java
@@ -0,0 +1,13 @@
+package com.metlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetlifeTeam5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetlifeTeam5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/metlife/MetlifeTeam5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetlifeTeam5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
